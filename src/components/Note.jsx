8ffa0c7ee0688ab7c9a664ae64273b377b110f56
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2021-11-29 10:25:54 -0300

    Challenge part 1. Done

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = () => {
+    return (
+        <div className="note">
+            <h1>Teste Título</h1>
+            <p>Teste conteúdo</p>
+        </div>
+    );
+};
+
+export default Note;
